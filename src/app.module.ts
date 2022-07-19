b49651242bfe484a4e3621d28cbf4b288536600a
betd-tcarbuccia <thomas.carbuccia@businessdecision.com>
2022-07-19 17:19:24 +0200

    Adding database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CatchablesModule } from './modules/catchables/catchables.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({ 
+      uri: `mongodb://${configService.get<string>('MONGO_INITDB_ROOT_USERNAME')}:${configService.get<string>('MONGO_INITDB_ROOT_PASSWORD')}@dinkum-online-db:27029/dinkum_online` 
+    }),
+    inject: [ConfigService]
+  }), CatchablesModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+ 
