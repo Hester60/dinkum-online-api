7c3e0a75a0405293f8e38fee94dc118d53457b90
Hester60 <thomascarbuccia@hotmail.fr>
2022-07-20 08:55:08 +0200

    Fixing service

--- src/modules/catchables/catchables.service.ts
+++ src/modules/catchables/catchables.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
-import {MongoRepository} from "typeorm";
+import {MongoRepository, Like} from "typeorm";
 import {Catchable} from "./catchables.entity";
 
 @Injectable()
