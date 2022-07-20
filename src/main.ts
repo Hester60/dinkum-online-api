f3d0e7141c380898ea44c7e795fc420b927df9be
Hester60 <thomascarbuccia@hotmail.fr>
2022-07-20 02:03:21 +0200

    cs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import {NestExpressApplication} from "@nestjs/platform-express";
-import { join } from 'path';
+import {join} from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public/images/previews'), {
-    prefix: '/public/images/previews/',
-  });
-  await app.listen(process.env.API_PORT);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.useStaticAssets(join(__dirname, '..', 'public/images/previews'), {
+        prefix: '/public/images/previews/',
+    });
+    await app.listen(process.env.API_PORT);
 }
+
 bootstrap();
