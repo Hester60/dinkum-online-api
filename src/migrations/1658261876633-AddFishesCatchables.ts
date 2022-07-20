ce96e1ab64718e83352d0a79b281b4b8ed5a144f
Hester60 <thomascarbuccia@hotmail.fr>
2022-07-20 02:10:20 +0200

    Moving data folder

--- src/migrations/1658261876633-AddFishesCatchables.ts
+++ src/migrations/1658261876633-AddFishesCatchables.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface} from "typeorm"
 import {MongoQueryRunner} from "typeorm/driver/mongodb/MongoQueryRunner";
-import catchables from '../data/catchables.json';
+import catchables from './data/catchables.json';
 
 export class AddFishesCatchables1658261876633 implements MigrationInterface {
 
