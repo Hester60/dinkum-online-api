789af27deee762aa28c46a947a95fad175963c6a
Hester60 <thomascarbuccia@hotmail.fr>
2022-07-22 13:27:09 +0200

    Adding bugs migrations

--- src/migrations/1658488990176-AddBugsCatchables.ts
+++ src/migrations/1658488990176-AddBugsCatchables.ts
@@ -1,8 +1,8 @@
 import {MigrationInterface} from "typeorm"
 import {MongoQueryRunner} from "typeorm/driver/mongodb/MongoQueryRunner";
-import catchables from './data/fishes.json';
+import catchables from './data/bugs.json';
 
-export class AddFishesCatchables1658261876633 implements MigrationInterface {
+export class AddBugsCatchables1658488990176 implements MigrationInterface {
 
     public async up(queryRunner: MongoQueryRunner): Promise<void> {
         await queryRunner.insertMany('catchables', catchables);
