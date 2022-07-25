968e571b0075efbe79ec74464e4c227e53d25e14
Hester60 <thomascarbuccia@hotmail.fr>
2022-07-25 18:11:06 +0200

    Adding SSL

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {NestExpressApplication} from "@nestjs/platform-express";
 import {join} from 'path';
+import fs from 'fs';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const httpsOptions = {
+        key: fs.readFileSync('./cert/privkey.pem'),
+        cert: fs.readFileSync('./cert/cert.pem'),
+    };
+
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        httpsOptions
+    });
     app.enableCors({
         origin: process.env.CORS_ORIGIN === '*' ?? process.env.CORS_ORIGIN.split(',')
     });
